14b2b1896ce394a0d6d60f9a62def158d513f105
Mike Rizzo <mrizzo@college.harvard.edu>
2013-11-03 16:06:12 -0500

    base structure

---
+++ html/js/scripts.js
@@ -0,0 +1,29 @@
+// slide in left
+jQuery.fn.slideRight = function () {
+    $(this).stop().animate({left:0,opacity: 1}, 500, function() {});
+    return this;
+}
+
+// slide out right
+jQuery.fn.slideLeft = function() {
+    $(this).stop().animate({left: -$(this).width(), opacity: 0},500, function() {});
+    return this;
+}
+
+$(document).ready(function(){
+  $(".toc").click(function(){
+    if ($(this).attr('active') == 'true') {
+      $(this).attr('active',false);
+      $(".left").slideLeft();
+    }
+    else {
+      $(this).attr('active',true);
+      $('.left').slideRight();
+    }
+  });
+
+  $(".container").click(function (){
+    $('.toc').attr('active',false);
+    $(".left").slideLeft();
+  });
+})
