3d665272c65b9b027c92a7696503e19ed5c7b85c
Mike Rizzo <mrizzo@college.harvard.edu>
2013-11-05 14:47:52 -0500

    sliding divs within the page

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -1,6 +1,12 @@
 // slide in left
 jQuery.fn.slideRight = function () {
+    $(this).css('left',-$(this).width());
     $(this).stop().animate({left:0,opacity: 1}, 500, function() {});
+    return this;
+}
+
+jQuery.fn.slideOutRight = function () {
+    $(this).stop().animate({left: $(document).width(),opacity: 1}, 500, function() {});
     return this;
 }
 
@@ -9,6 +15,13 @@ jQuery.fn.slideLeft = function() {
     $(this).stop().animate({left: -$(this).width(), opacity: 0},500, function() {});
     return this;
 }
+
+$("a").click(function(e){
+  e.preventDefault();
+  $('.active').slideOutRight().removeClass('active');
+  var location = $(this).attr('href');
+  $('#'+location).slideRight().addClass('active');
+})
 
 $(document).ready(function(){
   $(".toc").click(function(){
@@ -26,4 +39,6 @@ $(document).ready(function(){
     $('.toc').attr('active',false);
     $(".left").slideLeft();
   });
+  $('.content').css('left',-$(this).width()).show();
+  $('#start').slideRight().addClass('active');
 })
